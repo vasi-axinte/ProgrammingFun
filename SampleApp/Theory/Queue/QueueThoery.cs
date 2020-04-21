25fc353741b8097a8b1eb29d148543c8af1df5cf
Cristi <cristiaxinte20@gmail.com>
2020-04-21 22:09:04 +0300

    Added Queue and Stack Theory .NET App

---
+++ SampleApp/Theory/Queue/QueueThoery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theory.Queue  // Queue represents a first-in, first-out collection.
+                        // Imagine going to McDonalds drive. If you are the first in line, you will get out first 
+{
+    public class QueueThoery
+    {
+        public void Execute()
+        {
+            ShoppingList myList = new ShoppingList();
+            myList.List();              
+        }
+    }
+}
---
+++ SampleApp/Theory/Stack/StackTheory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Theory.Stack
+{ 
+    public class StackTheory    //Stack represents a last-in-first-out ( LIFO ) collection
+                        /*Imagine having a pillar of books, and u want to take the bottom book without
+                          destroying the pillar. So, u will have to remove the latest books that u put
+                          in the pillar to get to the bottom one. Like this, the first book added to the
+                          pillar ( the bottom one ) will be the last that comes out. */
+    {
+        public void Execute()
+        {
+            PillarOfBooks myBooks = new PillarOfBooks();
+            myBooks.Pillar();
+        }
+    }
+}
